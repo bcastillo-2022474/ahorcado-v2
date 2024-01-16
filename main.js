7a1eefb026041bb3042fa5f00fed291ed33e266f
Informatica <informatica@alumno.local>
2024-01-16 15:59:00 -0600

    #title commit 2 #comment logic working, enhancing UI

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+console.log('WORKING')
 const mainWordInput = document.querySelector('#mainWordInput');
 const inputsContainer = document.querySelector("#inputsContainer"); 
+const buttonSubmit = document.querySelector("#buttonSubmit");
+console.log(inputsContainer)
 // TODO: needs to set some words
 const possibleWords = [
   "Desarrollo",
@@ -105,44 +108,87 @@ function getRandomWord() {
 	return possibleWords[positionWord];
 }
 
-function printInputs(word, guessedLetters) {
+function printInputs(word, guessedLetters, wordWithCase) {
+	console.log('Existing content:', inputsContainer.innerHTML);
 	// clear the current content
-	inputsContainer.innerHtml = '<div class="flex justify-center gap-3">'
+	inputsContainer.innerHTML = '';
+	console.log('cleared content', inputsContainer.innerHTML);
+	console.log(word)
 	// re render content
 	for (let i = 0; i < word.length; i++) {
 		const currentLetter = word[i];
+		const currentLetterWithCase = wordWithCase[i];
+		console.log(guessedLetters.has(currentLetter), currentLetter)
 		if (guessedLetters.has(currentLetter)) {
 			// render the word
-			const inputLetter = `<div class="border p-3">${currentLetter.toUpperCase()}</div>`
-			inputsContainer.innerHtml+=inputLetter;
-			return;
+			const inputLetter = `<div class="border border-black p-3">${currentLetterWithCase}</div>`
+			inputsContainer.insertAdjacentHTML('beforeend', inputLetter);
+			continue;
 		}
 		// render empty input
-		const emptyInput = `<div class="border p-3"></div>`
-		inputsContainer.innerHtml+=emptyInput
+		const emptyInput = `<div class="border border-black p-3"></div>`
+		inputsContainer.insertAdjacentHTML('beforeend', emptyInput);
 	}
-	inputsContainer.innerHtml+='</div>'
 }
 
 function renderError() {
 	// TODO render some error;
+	bodyToColor('bg-red-100')
+
+}
+
+function checkIfHasWin(guessedLetters, word) {
+	if (guessedLetters.size === word.length) return true;
+	return false;
+}
+
+function bodyToColor(color) {
+	console.log('renderWin')
+	const body = document.querySelector('body');
+	console.log(body)
+	body.classList.add(color);
+	setTimeout(() => {
+		body.classList.remove(color);
+	}, 1000)
+}
+
+function renderWin() {
+	bodyToColor('bg-green-100');
 }
 
 function main() {
-	const word = getRandomWord()
+	console.log('main')
+	// const wordWithCase = getRandomWord();
+	const wordWithCase = 'a';
+	const word = wordWithCase.toLowerCase();
 	const guessedLetters = new Set();
-	printInputs(word, guessedLetters);
+	console.log({word, guessedLetters})
+	printInputs(word, guessedLetters, wordWithCase);
+	const submitContainer = document.querySelector('.submit-container');
 
-	mainWordInput.addEventListener("input", (e) => {
-		const letter = e.target.value;
+	buttonSubmit.addEventListener("click", (e) => {
+		const letter = mainWordInput.value.toLowerCase()
+		console.log(letter)
+
 		// check if it does exist in word
 		// updates guessedLetters also
-		if (word.contains(letter)) printInputs(word, guessedLetters);
+		if ([...word].includes(letter)) {
+			guessedLetters.add(letter)
+			printInputs(word, guessedLetters, wordWithCase);
+			// checkIfHasWin
+			const hasWon = checkIfHasWin(guessedLetters, word);
+			if (hasWon) renderWin()
+		}
 		else renderError();
 		// clean input
 		mainWordInput.value = '';
 	})
+	submitContainer.addEventListener('keypress', (e) => {
+		// if (e.key === 'enter') buttonSubmit.
+	})
 }
+
+main()
 
 
 
